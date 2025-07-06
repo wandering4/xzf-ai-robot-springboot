f4d9686e7492c25765af1067ba97dc69f95a640d
wandering_4 <17891997260@139.com>
2025-07-07 00:03:16 +0800

    first commit

---
+++ src/main/java/com/sankuai/ai/robot/XzfAiRobotSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sankuai.ai.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XzfAiRobotSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XzfAiRobotSpringbootApplication.class, args);
+    }
+
+}
